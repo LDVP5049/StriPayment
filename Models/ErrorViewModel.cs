d5ede872589043e384d2780c013d957209524694
LDVP5049 <luismaster752@gmail.com>
2023-10-24 16:33:08 -0700

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StriPayment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
